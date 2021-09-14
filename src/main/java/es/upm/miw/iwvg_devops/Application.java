483c213a381ad65b7ff7f188eb68797a0c32e4c2
Bruno Martin <=>
2021-09-14 11:47:21 +0200

    primer mensaje del primer commit que hago

---
+++ src/main/java/es/upm/miw/iwvg_devops/Application.java
@@ -0,0 +1,20 @@
+package es.upm.miw.iwvg_devops;
+
+import es.upm.miw.iwvg_devops.EjerciciosStream.Fraction;
+import es.upm.miw.iwvg_devops.EjerciciosStream.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
+import java.util.stream.Stream;
+
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class}) // Not API: /error
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("tes");
+        Fraction fraction = new Fraction();
+        Stream<User> stream = fraction.isProperFraction(1);
+        System.out.println(stream.count());
+        //SpringApplication.run(Application.class, args);// mvnw clean spring-boot:run
+    }
+}
---
+++ src/test/java/es/upm/miw/iwvg_devops/rest/SearchesTest.java
@@ -0,0 +1,56 @@
+package es.upm.miw.iwvg_devops.rest;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import es.upm.miw.iwvg_devops.EjerciciosStream.Searches;
+
+public class SearchesTest {
+
+    @Test
+    void testFindUserFamilyNameByUserNameDistinct() {
+        assertEquals(List.of("Torres"), new Searches().findUserFamilyNameByUserNameDistinct("Paula")
+                .collect(Collectors.toList()));
+    }
+
+    @Test
+    void testFindUserFractionNumeratorByFamilyName() {
+        assertEquals(List.of(2, 4, 0, 1, 1), new Searches().findFractionNumeratorByUserFamilyName("Torres")
+                .collect(Collectors.toList()));
+    }
+
+    @Test
+    void testFindFamilyNameByFractionDenominator() {
+        assertEquals(List.of("López", "Torres"), new Searches().findUserFamilyNameByFractionDenominator(2)
+                .collect(Collectors.toList()));
+    }
+
+    void testFindUserIdByAnyProperFraction() {
+    }
+
+    void testFindUserNameByAnyImproperFraction() {
+    }
+
+    void testFindUserFamilyNameByAllSignFractionDistinct() {
+    }
+
+    void testFindDecimalFractionByUserName() {
+    }
+
+    void testFindDecimalFractionBySignFraction() {
+    }
+
+    void testFindFractionAdditionByUserId() {
+    }
+
+    void testFindFractionSubtractionByUserName() {
+    }
+
+    void testFindFractionMultiplicationByUserFamilyName() {
+    }
+
+}
